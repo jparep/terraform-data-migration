6742365da440a996c2f7086697484bad84ac7349
jparep <parepson@gmail.com>
2025-03-06 20:14:52 -0700

    Created glue

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -6,3 +6,18 @@ resource "aws_glue_job" "convert_to_parquet" {
     script_location = "s3://${aws_s3_bucket.dms_s3_bucket.bucket}/glue-scripts/convert_to_parquet.py"
   }
 }
+
+
+# Glue job configuration
+resource "aws_glue_job" "convert_job" {
+  name     = "convert_to_parquet"
+  role_arn = aws_iam_role.glue_role.arn
+  glue_version = "3.0"
+  command {
+    script_location = "s3://vital-health-migration/scripts/convert_to_parquet.py"
+    python_version  = "3"
+  }
+  default_arguments = {
+    "--job-language" = "python"
+  }
+}
