fcc63a9cee8861af6a34a666b35ea6fede2ad1d1
jparep <parepson@gmail.com>
2025-03-06 20:14:27 -0700

    Createde eventbridge

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -8,3 +8,13 @@ resource "aws_cloudwatch_event_target" "glue_trigger" {
   target_id = "GlueJobTrigger"
   arn       = aws_lambda_function.trigger_glue_etl.arn
 }
+
+# Placeholder for AWS EventBridge configuration
+resource "aws_cloudwatch_event_rule" "trigger_rule" {
+  name        = "trigger-glue-job"
+  description = "Triggers Glue job on S3 event"
+  event_pattern = jsonencode({
+    source = ["aws.s3"],
+    detail-type = ["Object Created"]
+  })
+}
