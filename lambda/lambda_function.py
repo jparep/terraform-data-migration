a5f40803c51e3955ebe11c3b445281ba3f7ee2a7
jparep <parepson@gmail.com>
2025-03-23 23:36:49 -0600

    Updated convert to parquest

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO: Implement Lambda function logic
+    """Implement Lambda function to convert CSV to Parquet."""
     print("Lambda function triggered with event:", json.dumps(event))
     
     return {
